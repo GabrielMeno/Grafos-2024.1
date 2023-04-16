775eaa225c2791ca9a623634ddff7c6f2afa9660
Gabriel Meno <gabriel.menoncin@estudante.uffs.edu.br>
2023-04-16 18:22:53 -0300

    first commit

---
+++ tarefa_00_a_solucao/somador.cpp
@@ -0,0 +1,23 @@
+/*
+ * Tarefa de laboratorio 00 - Somador
+ *
+ * GEN254 - Grafos - 2023/1
+ *
+ * Nome:      Gabriel Henrique Menoncin
+ * Matricula: 2121101065
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a;
+    int b;
+
+    cin >> a >> b;
+
+    cout << (a + b) << "\n";
+
+    return 0;
+}
